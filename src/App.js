88a2481f9ffbe907433264e60a121639947183b9
Zo Nirina <tomzonirina@gmail.com>
2022-12-17 14:36:44 +0300

    I add Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './components/nav'
+import './index.css'
 
 export default function App() {
     return(
--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,16 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 export default function Nav() {
     return(
         <>
-            
+            <section>
+                <nav>
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                    </ul>
+                </nav>
+            </section>
         </>
     )
 }
