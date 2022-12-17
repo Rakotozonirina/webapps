5468ad012fab813beb7c0b4795d6e6f113455225
Zo Nirina <tomzonirina@gmail.com>
2022-12-17 17:01:29 +0300

    I install the hero page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/nav'
+import Hero from './components/hero'
 import './index.css'
 
 export default function App() {
     return(
         <>
             <Nav/>
+            <Hero/>
         </>
     )
 }
