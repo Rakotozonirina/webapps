9020d0181106d80708ad585c204c1152bdef909e
Zo Nirina <tomzonirina@gmail.com>
2022-12-17 15:41:16 +0300

    I add navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,14 +5,15 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 export default function Nav() {
     return(
         <>
-            <section>
+            <section className='home'>
                 <nav>
                     <ul>
-                        <li><a href="#">HOME</a></li>
-                        <li><a href="#">ABOUT</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">CONTACT</a></li>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">A propos</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Contact</a></li>
                     </ul>
+                    <button>Connexion</button>
                 </nav>
             </section>
         </>
