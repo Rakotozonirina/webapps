7f0393777ffbde4bf5c7343295a1867acc0052e0
DavidGabrielo <99davidgab@gmail.com>
2022-12-17 23:51:47 +0100

    Changement de footer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import "./index.css";
 import Sary from "./photos/gauche.png";
+import Footer from './components/footer';
 
 class App extends Component {
     render() {
         return (
-            <div className='conteneur'>
-                <img src={Sary} alt="" className='gauche' />
-                <div>
-                    <h2>Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.</h2>
-                    <p>Pour faire des etude et des recherche
-                        des besoin</p>
-                    <button className='bienvenu'>Bienvenu</button>
+            <div>
+                <div className='conteneur'>
+                    <img src={Sary} alt="" className='gauche' />
+                    <div>
+                        <h2>Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit.</h2>
+                        <p>Pour faire des etude et des recherche
+                            des besoin</p>
+                        <button className='bienvenu'>Bienvenu</button>
+                    </div>
                 </div>
+                <Footer />
             </div>
         )
     }
