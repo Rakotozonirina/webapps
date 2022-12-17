c342d6b62927528a6cd25fab9139abd164e9239b
kananavy <kananavymiremby@gmail.com>
2022-12-17 22:20:16 +0300

    mbola1

--- src/components/about.js
+++ src/components/about.js
@@ -7,20 +7,35 @@ export default function About() {
         <section className="about">
           <div className="a">
             <div className="back1"></div>
-           <h1>Lieu de loisir</h1>
+           <h1>🏃‍♂️ Lieu de loisir</h1>
            <h2>Lorem ipsum dolor sit amet,<br></br> 
                 consectetur adipiscing elit. <br></br>
                 Nunc vulputate libero et velit<br></br>
                 interdum, ac aliquet odio mattis.</h2>
           </div>
           <div className="b">
-          <h1>rachel</h1>
+          <div className="back2"></div>
+          <h1 className="schr">🛍 Recherche du travail</h1>
+           <h2>Lorem ipsum dolor sit amet,<br></br> 
+                consectetur adipiscing elit. <br></br>
+                Nunc vulputate libero et velit<br></br>
+                interdum, ac aliquet odio mattis.</h2>
             </div>
             <div className="c">
-            <h1>kananavy</h1>
+            <div className="back3"></div>
+            <h1 className="schr2">📚 Recherche de documents</h1>
+           <h2 className="oo">Lorem ipsum dolor sit amet,<br></br> 
+                consectetur adipiscing elit. <br></br>
+                Nunc vulputate libero et velit<br></br>
+                interdum, ac aliquet odio mattis.</h2>
             </div>
             <div className="d">
-            <h1>miremby</h1>
+            <div className="back4"></div>
+            <h1>🏃‍♂️ Lieu de loisir</h1>
+           <h2>Lorem ipsum dolor sit amet,<br></br> 
+                consectetur adipiscing elit. <br></br>
+                Nunc vulputate libero et velit<br></br>
+                interdum, ac aliquet odio mattis.</h2>
             </div>
         </section>
         </>
