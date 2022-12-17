d4eb59df69139a914f917f298d038609b6c50d2f
kananavy <kananavymiremby@gmail.com>
2022-12-17 14:28:46 +0300

    ity about aloha

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Nav from './components/nav'
+import About from './components/about'
+import './index.css'
 
 export default function App() {
     return(
         <>
             <Nav/>
+            <About/>
         </>
     )
 }
---
+++ src/components/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function About() {
+    return(
+        <>
+        <section className="about">
+          <div className="a">
+           <h1>elio</h1>
+          </div>
+          <div className="b">
+          <h1>rachel</h1>
+            </div>
+            <div className="c">
+            <h1>kananavy</h1>
+            </div>
+            <div className="d">
+            <h1>miremby</h1>
+            </div>
+        </section>
+        </>
+    )
+}
