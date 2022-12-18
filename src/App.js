9231c2c8215d0baf4690838d605e6008e77fdd95
Rakotozonirina <96328295+Rakotozonirina@users.noreply.github.com>
2022-12-18 03:54:18 +0300

    Apropos (#5)
    * Interface de a propos
    
    * Enlevement des children
    
    * Changement de footer
    
    Co-authored-by: DavidGabrielo <99davidgab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
-import Nav from './components/nav'
-import About from './components/about'
-import Hero from './components/hero'
-import './index.css'
+import React from 'react';
+import Nav from './components/nav';
+import About from './components/about';
+import Hero from './components/hero';
+import { render } from '@testing-library/react';
+import "./index.css";
+import Sary from "./photos/gauche.png";
+import Footer from './components/footer';
+
+
 export default function App() {
     return(
         <>
             <Nav/>
             <Hero/>
             <About/>
+            <div>
+                <div className='conteneur'>
+                    <img src={Sary} alt="" className='gauche' />
+                    <div>
+                        <h2>Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit.</h2>
+                        <p>Pour faire des etude et des recherche
+                            des besoin</p>
+                        <button className='bienvenu'>Bienvenu</button>
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </>
     )
 }
+
